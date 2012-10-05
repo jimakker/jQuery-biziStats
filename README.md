5bb1d6240cbdabb8419c513f26f30d5f79083127
Jimakker <tiburtzio@gmail.com>
2012-10-05 20:50:45 +0200

    Eredua, parametroak eta @julen-ek gomendatutako aldaketak

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ Erabili
 * Zure web orriari gehitu (jQuery behar duzu!)
 * div huts bat sortu eta nahi duzun id-a eman (`<div id="nireID"></div>`)
 * `$("#nireID").biziStats()` exekutatu
+* Parametro batzuk pasa daitezke objektu eran nahi bada:
+	* refresh (Boolean) true bada minuturo eguneratuko da plugina
+	* stationId (Integer) 1-9, geltoki bakar baten informazioa nahi bada
+	* `$('#nireID').biziStats({refresh:true,staionId:5})`
+eredua.html fitxategian erabilera ereduak daude
--- jquery.biziStats.js
+++ jquery.biziStats.js
@@ -14,20 +14,21 @@
     if(options){$.extend( settings, options );}
   
     var apiUrl = "http://bizistats.info/api/last?callback=?";  
-    var stations = [];
+  
     function init(){
+        var stations = [];
         console.log('init');
         $.getJSON(apiUrl, function(data) {
             for(var i = 0; i<data.stations.length;i++){
                 if(settings.stationId){
                     if(data.stations[i].stationId==settings.stationId){
-                        var stationLi = '<li>'+data.stations[i].name+': '+
-                        data.stations[i].currentNum+'/'+data.stations[i].totalNum+'</li>';
+                        var stationLi = ['<li>',data.stations[i].name,': ',
+                        data.stations[i].currentNum,'/',data.stations[i].totalNum,'</li>'].join('');
                         stations.push(stationLi);
                     }
                 } else {
-                    var stationLi = '<li>'+data.stations[i].name+': <span class="bizistats-station-'+ data.stations[i].stationId +'">'+
-                    data.stations[i].currentNum+'/'+data.stations[i].totalNum+'</span></li>';
+                    var stationLi = ['<li>',data.stations[i].name,': <span class="bizistats-station-', data.stations[i].stationId,'">',
+                    data.stations[i].currentNum,'/',data.stations[i].totalNum,'</span></li>'].join('');
                     stations.push(stationLi);
                 }
 
@@ -41,9 +42,9 @@
     init();
     if(settings.refresh){
         setInterval(function(){
-            $this.html('');
+            $this.empty();
             init();
-        },10000);
+        },60000);
     } 
     return this;
   };
